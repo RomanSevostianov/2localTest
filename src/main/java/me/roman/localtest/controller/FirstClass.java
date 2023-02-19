14760ea5b972243e987d1506d7ca4903e900ca88
Роман <pangujailbreake58@gmail.com>
2023-02-19 15:24:18 +0300

    1

--- src/main/java/me/roman/localtest/controller/FirstClass.java
+++ src/main/java/me/roman/localtest/controller/FirstClass.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FirstClass {
 
+
+
     @GetMapping
     public String start() {
         return "Приложение запущено!";
