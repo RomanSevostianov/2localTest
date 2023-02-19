8ead5613617c3c8bd1578ad28ba9e693f719c8e6
Роман <pangujailbreake58@gmail.com>
2023-02-19 15:13:34 +0300

    Initial commit

---
+++ src/test/java/me/roman/localtest/LocalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package me.roman.localtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
